f544991a46f52d9760c9c890e0d877e0672d04ee
Zhang Jiekai <zjkisgood@126.com>
2023-02-16 15:38:00 +0800

    finish backend

---
+++ backend/main.py
@@ -0,0 +1,37 @@
+import pymongo
+from fastapi import FastAPI
+from pydantic import BaseModel
+from bson.json_util import dumps
+from typing import Optional
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017")
+collection = myclient['test']['data']
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    province: str
+    city: Optional[str]
+    email: Optional[str]
+    update_time: Optional[str]
+
+    def to_dict(self):
+        return {
+            'name': self.name, 
+            'province': self.province, 
+            'city': self.city, 
+            'email': self.email, 
+            'update_time': self.update_time, 
+        }
+    
+
+@app.get("/all")
+async def all():
+    return dumps(collection.find())
+
+@app.post("/insert")
+async def insert(item: Item):
+    collection.delete_many({'name': item.name})
+    collection.insert_one(item.to_dict())
+    return dumps(collection.find())
